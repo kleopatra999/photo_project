39a797d4fa20ff2d96c33b62b9df4b64c1c6f2f4
Matt Oakes <matt@ribot.co.uk>
2012-12-09 18:25:45 +0000

    Added a basic drag box to the photo list

--- public/js/views/PhotoListView.js
+++ public/js/views/PhotoListView.js
@@ -2,9 +2,10 @@ App.views = App.views || {};
 
 App.views.PhotoListView = Backbone.View.extend({
     template: Handlebars.compile($('#photoListViewTemplate').html()),
-    
+    $dropzone: null,
+
     initialize: function(options) {
-        _.bindAll(this, 'render');
+        _.bindAll(this, 'render', '_handleDragLeave', '_handleDragOver', '_handleDrop');
         this.setCollection = options.setCollection;
 
         this.collection.bind('reset', this.render);
@@ -12,6 +13,7 @@ App.views.PhotoListView = Backbone.View.extend({
     },
 
     render: function() {
+        // Set the html
         var sets = this.setCollection.toJSON();
         var set = (sets) ? sets[0] : null;
         var photos = this.collection.toJSON();
@@ -19,6 +21,34 @@ App.views.PhotoListView = Backbone.View.extend({
             set: set,
             photos: photos
         }));
+
+        // Set the drag and drop listeners
+        this.$dropzone = this.$el.find('#dropzone');
+        var dropzone = this.$dropzone.get(0); // Gets the DOM element from jQuery. Not sure if this is actually quicker
+        dropzone.addEventListener('dragleave', this._handleDragLeave, false);
+        dropzone.addEventListener('dragover', this._handleDragOver, false);
+        dropzone.addEventListener('drop', this._handleDrop, false);
+
         return this;
+    },
+
+    _handleDragLeave: function(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        this.$dropzone.removeClass('dragging');
+    },
+    _handleDragOver: function(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = "copy";
+
+        this.$dropzone.addClass('dragging');
+    },
+    _handleDrop: function(evt) {
+        evt.stopPropagation();
+        evt.preventDefault();
+
+        this.$dropzone.removeClass('dragging');
     }
 });
