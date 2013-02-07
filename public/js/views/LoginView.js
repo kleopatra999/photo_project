937fa0e5cab2da77d42b5355d6dbfa87debba991
Matt Oakes <matt@ribot.co.uk>
2013-02-07 15:48:55 +0000

    Added a method which makes the login with the server

--- public/js/views/LoginView.js
+++ public/js/views/LoginView.js
@@ -3,6 +3,10 @@ App.views = App.views || {};
 App.views.LoginView = Backbone.View.extend({
     template: Handlebars.compile($('#loginViewTemplate').html()),
 
+    events: {
+        'click .loginSubmit': '_loginSubmitClicked'
+    },
+
     initialize: function() {
         _.bindAll(this, 'render');
     },
@@ -10,6 +14,14 @@ App.views.LoginView = Backbone.View.extend({
     render: function() {
         this.$el.html(this.template());
         return this;
+    },
+
+    _loginSubmitClicked: function() {
+        var email = this.$el.find('input#email').val();
+        var password = this.$el.find('input#password').val();
+        console.log('Should now login...', email, password);
+        App.dataController.login(email, password);
+        return false;
     }
 });
 
